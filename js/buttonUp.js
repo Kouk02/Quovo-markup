ccbc9d5e22fcf9596028c2130fcbbd976673412a
kouk02 <kouk20020703@gmail.com>
2024-06-11 02:50:30 +0300

    update (js button Up)

--- js/buttonUp.js
+++ js/buttonUp.js
@@ -1,25 +1,28 @@
 let scrollToTopBtn = document.querySelector(".button-up");
+let prevScrollTop = 0;
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 100) {
         scrollToTopBtn.style.display = "block";
-        setTimeout(() => {
-            scrollToTopBtn.classList.add("_anim-items");
-            scrollToTopBtn.classList.remove("_inactive");
-        }, 10); 
+        scrollToTopBtn.classList.add("_anim-items");
+        scrollToTopBtn.classList.remove("_inactive");
+
+        if (scrollTop > prevScrollTop) {
+            scrollToTopBtn.classList.add("scroll-down");
+            scrollToTopBtn.classList.remove("scroll-up");
+        } else {
+            scrollToTopBtn.classList.add("scroll-up");
+            scrollToTopBtn.classList.remove("scroll-down");
+        }
     } else {
         scrollToTopBtn.classList.add("_inactive");
         scrollToTopBtn.classList.remove("_anim-items");
-        setTimeout(() => {
-            scrollToTopBtn.style.display = "none";
-        }, 800); 
+        scrollToTopBtn.style.display = "none";
     }
-}
 
-
-function scrollToTop() {
-    document.body.scrollTop = 0; // Для Safari
-    document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE та Opera
+    prevScrollTop = scrollTop;
 }
