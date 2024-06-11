d008e1ab7dc8e7e5df37a22a6b47b4992c6728cb
kouk02 <kouk20020703@gmail.com>
2024-06-11 03:19:17 +0300

    update animation
    Header

--- js/animations.js
+++ js/animations.js
@@ -37,3 +37,44 @@ if (animItems.length > 0) {
         animOnScroll();
     }, 300);
 }
+
+//! ////////////////////////////////////////animation header
+let animItemsLogo = document.querySelectorAll('._anim-items-logo ');
+
+if (animItemsLogo.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+
+    function animOnScroll() {
+        for (let index = 0; index < animItemsLogo.length; index++) {
+            const animItem = animItemsLogo[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+            
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active-logo');
+                animItem.classList.remove('_inactive-logo');
+            } else if (pageYOffset < animItemOffset - animItemHeight || pageYOffset > (animItemOffset + animItemHeight)) {
+                animItem.classList.remove('_active-logo');
+                animItem.classList.add('_inactive-logo');
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+              scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+              scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
